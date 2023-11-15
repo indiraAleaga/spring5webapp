21d24c079cdf59702322020d15581e1188d8c618
e-bravodey <deysi.bravo.extern@diconium.com>
2023-11-15 22:07:30 +0100

    Adding book view

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring5webapp.controller;
+package guru.springframework.spring5webapp.controllers;
 
 import guru.springframework.spring5webapp.services.BookService;
 import org.springframework.stereotype.Controller;
